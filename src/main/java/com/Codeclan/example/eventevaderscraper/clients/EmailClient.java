c5d8fa475511a787053c0bd40e57c1826d85086d
Azhar Sharif <azhar981@gmail.com>
2020-02-22 10:28:08 +0000

    Creating email service with javax mail

---
+++ src/main/java/com/Codeclan/example/eventevaderscraper/clients/EmailClient.java
@@ -0,0 +1,16 @@
+package com.Codeclan.example.eventevaderscraper.clients;
+
+import com.sun.mail.smtp.SMTPTransport;
+
+import javax.mail.Message;
+import javax.mail.MessagingException;
+import javax.mail.Session;
+import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeMessage;
+import java.util.Date;
+import java.util.Properties;
+
+public class EmailClient {
+
+
+}
