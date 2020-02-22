151a69969358f8528f888755f43b91ab95489de9
Azhar Sharif <azhar981@gmail.com>
2020-02-22 10:19:25 +0000

    Merge remote-tracking branch 'origin/alan' into azhar
    # Conflicts:
    #	db/eventservice_db.mv.db
    #	db/eventservice_db.trace.db
    #	src/test/java/com/Codeclan/example/eventevaderscraper/EventevaderscraperApplicationTests.java

--- src/main/java/com/Codeclan/example/eventevaderscraper/clients/EventClient.java
+++ src/main/java/com/Codeclan/example/eventevaderscraper/clients/EventClient.java
@@ -1,5 +1,6 @@
 package com.Codeclan.example.eventevaderscraper.clients;
 
+import com.Codeclan.example.eventevaderscraper.models.Event;
 import org.springframework.http.*;
 import org.springframework.stereotype.Component;
 import org.springframework.web.client.RestTemplate;
@@ -12,13 +13,13 @@ public class EventClient {
 
     public ResponseEntity<String> requestEvents(){
 
-        String url = "https://api.list.co.uk/search?query=glasgow";
+        String url = "https://api.list.co.uk/v1/search?query=glasgow";
 
         RestTemplate restTemplate = new RestTemplate();
 
         HttpHeaders headers = new HttpHeaders();
 
-        headers.set("Authorization", "Bearer" + "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyX2lkIjoiOWRhZjc2MjAtOGEwOS00ZDY3LThkOTItOGE5Y2ViNTBhODRkIiwia2V5X2lkIjoiZDA5ZWJkZjUtYzk5My00OGY2LWEwZjgtOGZkNTg3ZDZjY2E5IiwiaWF0IjoxNTgyMjE4NjA4fQ.ghY5tAbaV8nm5Sb9gFe5B7XauQnY6_BvalWq5ARyHUY");
+        headers.set("Authorization", "Bearer " + "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyX2lkIjoiOWRhZjc2MjAtOGEwOS00ZDY3LThkOTItOGE5Y2ViNTBhODRkIiwia2V5X2lkIjoiZDA5ZWJkZjUtYzk5My00OGY2LWEwZjgtOGZkNTg3ZDZjY2E5IiwiaWF0IjoxNTgyMjE4NjA4fQ.ghY5tAbaV8nm5Sb9gFe5B7XauQnY6_BvalWq5ARyHUY");
 
         HttpEntity request = new HttpEntity(headers);
 
@@ -28,4 +29,10 @@ public class EventClient {
 
         return response;
     }
+
+//    public Event requestEvent(){
+//        RestTemplate restTemplate = new RestTemplate();
+//        ResponseEntity<String> allEvents = requestEvents();
+//
+//    }
 }
