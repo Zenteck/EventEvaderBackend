bc24dfdbb67bc7e3e1ae61bf79969a97dd41ad46
Azhar Sharif <azhar981@gmail.com>
2020-02-25 16:22:03 +0000

    Got send all emails working

--- src/main/java/com/Codeclan/example/eventevaderscraper/EventevaderscraperApplication.java
+++ src/main/java/com/Codeclan/example/eventevaderscraper/EventevaderscraperApplication.java
@@ -1,16 +1,16 @@
 package com.Codeclan.example.eventevaderscraper;
 
+import com.Codeclan.example.eventevaderscraper.clients.SendGridEmailService;
 import com.Codeclan.example.eventevaderscraper.payloads.Email;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.Scheduled;
 
 @SpringBootApplication
 public class EventevaderscraperApplication {
 
 	public static void main(String[] args) {
-
-
-
+		SendGridEmailService sendGridEmailService;
 		SpringApplication.run(EventevaderscraperApplication.class, args);
 
 
